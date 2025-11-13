c1d09fa847d8b9b06573af60f5e56f5529be8a82
Woeter <skippedlead@gmail.com>
2025-11-13 22:24:38 +0530

    Addded Numpy matrices for easier calulations

--- src/pagerank.py
+++ src/pagerank.py
@@ -19,52 +19,62 @@ def pagerank(g,rounds,d = 0.85):
 
     
     n = len(all_nodes) # Basically getting hte number of nodes present
+    
+    node_map = {}
+    
+    i = 0
 
-    initial_rank = 1/n # All of the nodes get the same rank using this
+    for node in all_nodes:
+        node_map[node] = i
+        i+=1
+
 
     if not n:
         return {} # If there are no nodes return nothing
-
-    pagerank = {} # A map to store {node_name: rank}
-
-    base_score = (1-d)/n # Just a calculated guess what if the user gets bored and don't go through one of the links and rather jumps to a random page (Its a escape route that also helps in completing the probability equation)
     
-    for node in all_nodes:
+    voteMatrix = np.zeros((n, n))
 
-        pagerank[node] = initial_rank # Assigning all nodes the same rank
+    for j_node in g.nodes():
+        out_degree = g.out_degree(j_node)
+        
+        if out_degree > 0:
+            for i_node in g.successors(j_node):
 
-    pprint.pprint(pagerank) # Checking if it worked
+                i = node_map[i_node]
+                j = node_map[j_node]
 
-    prev_pagerank = pagerank # As we have to keep track of the previous state of our ranks
+                voteMatrix[i,j] = 1.0 / out_degree
+
+
+    print("Vote Matrix")
+    print(voteMatrix, '\n')
+
+
+
+    scoreMatrix = np.ones(n)/n
+
+
+    baseRankVector = np.ones(n) * ((1-d)/n) # Just a calculated guess what if the user gets bored and don't go through one of the links and rather jumps to a random page (Its a escape route that also helps in completing the probability equation)
+    
 
     print (f"\nDoing {rounds} interations\n")
 
     for i in range(rounds): # The reason we go through multiple iterations is because its like the what comes first egg or chicken puzzle The score of A depend of C and score of C depend on A so as we go thorugh many iteration the value gets closer and closer to to the value it is supposed to be. Also a quick note as soon as the score start not to change by very much we should stop the iterations.
 
-        new_pagerank = {} # Resetting the map to store new values
-
-        for node in all_nodes:
-            links_score_total = 0 # Starting scores given by nodes will be 0 
-
-
-            for link in g.predecessors(node): # Gets the nodes that are connected to our current node 
-                link_rank = prev_pagerank[link] # Gets the score from the last interation
-
-                out_edges = g.out_degree(link) # Gets how many edges gets out of our current node (we're basically distributing its score to its connected nodes)
-
-                given_score = (link_rank / out_edges) # Formula derived by the pagerank founders (A way to calculate the Link score a number represting how often a person would stumble on a page going through by links)
-
-                links_score_total += given_score # We sum it up as there could be multiple nodes connnected to our current node
-
-            new_pagerank[node] = (base_score + d * (links_score_total)) # Using the main formula dervied by the founders of pagerank again the base score is just a number for the probability a person would come to a page NOT through by links
-
-        prev_pagerank =  pagerank # Giving the current data for the next iteration as current will be past for next iteration
-        pagerank = new_pagerank # Given our main map 'pagerank' the scores we collected
-
+        scoreMatrix = baseRankVector + d * (voteMatrix @ scoreMatrix)
 
     print("Complete! The interations are done!\n")
 
-    return pagerank 
+    pagerank = {}
+
+    for node in node_map:
+        i = node_map[node]
+
+        score = scoreMatrix[i]
+
+        pagerank[node] = score
+
+    return pagerank
     
 
 g = nx.DiGraph()
